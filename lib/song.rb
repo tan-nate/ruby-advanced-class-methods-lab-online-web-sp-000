1b9d5d5e072041d5a9b8f6bb2cded8b7fb2b4f12
Nate Tan <nktan93@gmail.com>
2019-04-02 21:34:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -13,16 +11,58 @@ class Song
   end
   
   def self.create
-    self.new.save
-    self.all.last
+    song = self.new
+    song.save
+    song
   end
   
   def self.new_by_name(name)
     song = self.new
-    #binding.pry
     song.name = name
-    #binding.pry
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    split_filename = filename.split(/[-.]/)
+    stripped_filename = split_filename.collect{|string| string.strip}
+    stripped_filename.delete("mp3")
+    artist_name = stripped_filename[0]
+    name = stripped_filename[1]
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
   end
 end
-
-#binding.pry
